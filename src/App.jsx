02ba23c9dd8f881d6659e561417dc254419b3e2a
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-05-28 16:34:40 +0530

    Browers Routing Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Header from './Components/Includes/Header'
 import Footer from './Components/Includes/Footer'
+import Home from "./Components/Home/Home";
+import { Route , Routes } from 'react-router-dom';
+
+
 export default function App() {
   return (
     <>
       <Header/>
-      <div>Main App</div>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route>404 Not Found !</Route>
+        </Routes>
       <Footer/>
     </>
   )
--- src/index.js
+++ src/index.js
@@ -2,10 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'; 
 
+// Vendors Css
+import 'bootstrap/dist/css/bootstrap.css'
+import './assets/css/fancybox.min.css'
+import './assets/css/slick.css'
+import './assets/css/slick-theme.css'
+import './assets/css/aos.css'
+import './assets/css/jquery.mCustomScrollbar.min.css'
+import './assets/css/header.css'
+import './assets/css/main.css'
+import './assets/css/footer.css'
+import { BrowserRouter } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
