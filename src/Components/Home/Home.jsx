13c1f9f81c98a87a8924ae1bf276a8a7b334f157
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-05-28 17:31:05 +0530

    Home Page Update

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,9 +3,14 @@ import Banners from './Sections/Banners'
 import BookedTestSliders from './Sections/BookedTestSliders'
 import CheckupsSliders from './Sections/CheckupsSliders'
 import PackagesSliders from './Sections/PackagesSliders'
+import ConditionsSliders from './Sections/ConditionsSliders'
 
-
-
+import HowItsWorks from './Sections/HowItsWorks'
+import AboutUs from './Sections/AboutUs'
+import FindLocation from './Sections/FindLocation'
+import LegacyNutshell from './Sections/LegacyNutshell'
+import LatestBlogs from './Sections/LatestBlogs'
+import Accretions from './Sections/Accretions' 
 
 export default function Home() {
   return (
@@ -14,6 +19,13 @@ export default function Home() {
       <BookedTestSliders />
       <CheckupsSliders />
       <PackagesSliders />
+      <ConditionsSliders />
+      <HowItsWorks />
+      <AboutUs />
+      <FindLocation />
+      <LegacyNutshell />
+      <LatestBlogs />
+      <Accretions />
     </>
   )
 }
