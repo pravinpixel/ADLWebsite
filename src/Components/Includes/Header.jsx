bbb7e11cf2ee6c75f4e1db45209a367d38284026
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-05-28 16:40:52 +0530

    Header Components

--- src/Components/Includes/Header.jsx
+++ src/Components/Includes/Header.jsx
@@ -1,7 +1,116 @@
 import React from 'react'
+import logo from './../../assets/images/logo.png';
+import location from './../../assets/images/location.png';
+import sky from './../../assets/images/sky.png';
+import user from './../../assets/images/user.png';
+import cart from './../../assets/images/cart.png';
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <header id="header" className="main-header site-navbar">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <nav className="navbar navbar-default navbar-trans navbar-expand-lg">
+              <a className="navbar-brand text-brand" href="./"><img src={logo} alt="" className="img-fluid"/></a>
+              <div className="navbar-collapse collapse justify-content-end" id="navbarDefault">
+                <div className="row align-items-center">
+                  <div className="col">
+                    <ul className="top-links text-center">
+                      <li>
+                        <input type="text" placeholder="Search for Health Packages / Tests / Labs" />
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="col">
+                    <ul className="icns-lst text-center">
+                      <li>
+                        <a href="#">
+                          <img src={location} alt="" className="img-fluid"/>
+                          <span>Banglore</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="">
+                          <img src={sky} alt="" className="img-fluid"/>
+                          <span>Report</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="">
+                          <img src={user} alt="" className="img-fluid"/>
+                          <span>Login</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="">
+                          <img src={cart} alt="" className="img-fluid"/>
+                          <span>Cart</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col">
+                    <ul className="navbar-nav">
+                      <li className="nav-item">
+                        <a className="nav-link active" href="./">Home</a>
+                      </li>
+                      <li className="nav-item">
+                        <a className="nav-link" href="./">For Patients</a>
+                      </li>
+                      <li className="nav-item">
+                        <a className="nav-link" href="./">For Doctors</a>
+                      </li>
+                      <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Health Checkup</a>
+                        <ul className="dropdown-menu">
+                          <li>
+                            <a className="dropdown-item" href=" ">Checkup - 1</a>
+                          </li>
+                          <li>
+                            <a className="dropdown-item" href=" ">Checkup - 2</a>
+                          </li>
+                          <li>
+                            <a className="dropdown-item" href=" ">Checkup - 3</a>
+                          </li>
+                          <li>
+                            <a className="dropdown-item" href=" ">Checkup - 4</a>
+                          </li>
+                          <li>
+                            <a className="dropdown-item" href=" ">Checkup - 5</a>
+                          </li>
+                          <li>
+                            <a className="dropdown-item" href=" ">Checkup - 6</a>
+                          </li>
+                          <li>
+                            <a className="dropdown-item" href=" ">Checkup - 7</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li className="nav-item">
+                        <a className="nav-link" href="">Find a lab</a>
+                      </li>
+                      <li className="nav-item">
+                        <a className="nav-link" href="">About ADL</a>
+                      </li>
+                      <li className="nav-item">
+                        <a className="nav-link" href="">Reach Us</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarDefault" aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation">
+                <span></span>
+                <span></span>
+                <span></span>
+              </button>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
