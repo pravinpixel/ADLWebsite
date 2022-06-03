3059321a921d6a6f5024b7f25d16f61e4c097939
behin-pixel <behin.pixel@gmail.com>
2022-06-03 11:52:05 +0530

    Test detail apge changes

--- src/Components/Containers/TestDetails.jsx
+++ src/Components/Containers/TestDetails.jsx
@@ -8,6 +8,10 @@ import {
 } from "../../Redux/Actions/TestAction";
 import { API_URL } from "../../Redux/Constant/ApiRoute";
 import { Link } from "react-router-dom";
+import testicon1 from '../../assets/images/testing-icon-1.png'
+import testicon2 from '../../assets/images/testing-icon-2.png'
+import testicon3 from '../../assets/images/testing-icon-3.png'
+import testicon4 from '../../assets/images/testing-icon-4.png'
 
 export default function TestDetails() {
   const { TestId } = useParams();
@@ -31,7 +35,8 @@ export default function TestDetails() {
   return (
     <>
       {testDetails !== undefined ? (
-        <section className="comon-testdetail-banner">
+        <>
+          <section className="comon-testdetail-banner">
           <div className="container">
             <div className="row">
               <div className="col">
@@ -55,6 +60,55 @@ export default function TestDetails() {
             </div>
           </div>
         </section>
+
+        <section className="testing-details text-left">
+          <div className="container">
+            <div className="row">
+              
+              <div className="col-lg-7">
+                <div className="commentestng-heads">
+                  <h2><span>Test ID - ACE</span>Angiotensin Converting Enzyme (ACE), Serum</h2>
+                </div>
+                <div className="testng-details">
+                  <ul>
+                    <li>
+                      <img src={testicon1} alt="call" className="img-fluid"/>
+                      <span>Basic Instruction </span>
+                      2 mL (1 mL min.) serum from 1 SST.
+                    </li>
+                    <li>
+                      <img src={testicon2} alt="call" className="img-fluid"/>
+                      <span>Test Shedule</span>
+                      Monday, Wednesday, Saturday.
+                    </li>
+                    <li className="colap-seing">
+                      <img src={testicon3} alt="call" className="img-fluid"/> 
+                        <h5>
+                        &#8377;1400{" "}
+                        <span className="strke">
+                        <s>&#8377;1556</s>
+                        </span>
+                        <span className="flag-option">
+                        <p>10 % off</p>
+                        </span>
+                      </h5>
+                    </li>
+                    <li>
+                      <img src={testicon4} alt="call" className="img-fluid"/> 
+                      This test is eligible for Home Collection.
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              
+              <div className="col-lg-5">
+
+              </div>
+
+            </div>
+          </div>
+        </section>
+        </>
       ) : null}
     </>
   );
