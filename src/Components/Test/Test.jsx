352c3ddb480339a96f335807bd086cc63d0272c2
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-06-10 13:39:12 +0530

    Sorting Filter

--- src/Components/Test/Test.jsx
+++ src/Components/Test/Test.jsx
@@ -1,18 +1,61 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import TestBanner from './Sections/TestBanner' 
 import PopularTests from './Sections/PopularTests'
 import PopularPackages from './Sections/PopularPackages'
 import TestHealthPackages from './Sections/TestHealthPackages'
-
+import { setAllTestDetails, setTestCartList } from '../../Redux/Actions/TestAction'
+import { AddToCartList } from "../../../src/Helpers";
+import { API_URL } from '../../Redux/Constant/ApiRoute'
+import axios from 'axios'
+import { useDispatch, useSelector } from 'react-redux'
 export default function test() {
+
+  const dispatch                 = useDispatch();
+  const testListing              = useSelector((state) => state.TestList.testList);
+  const [tackTest, setTackTest]  = useState(8);
+  const [sortBy, setSortBy]      = useState('high');
+  const [loader, setLoader]      = useState(true);
+  const [search , setSearch]     = useState(null);
+
+  const getAllTest = (sortBy, search , tackTests) => { 
+    setLoader(true)
+    axios.post(API_URL.TEST_LISTS, { 
+      search : search !== null ? search : null,
+      tack   :tackTests !== undefined ? tackTests : tackTest,
+      sort   : sortBy
+    }).then((response) => {
+      dispatch(setAllTestDetails(response.data.data));
+      setLoader(false)
+    });
+  }; 
+  const sortByPrice = (sortingValue) => {
+    getAllTest(sortingValue, search);
+  }
+  const addTestToCart = (testDetails) => {
+    AddToCartList(testDetails)
+    dispatch(setTestCartList(JSON.parse(localStorage.getItem('CartTestList'))));
+  };
   useEffect(() => {
     document.title = "Test Details Page";
     window.scroll(0,0)
+    getAllTest();
   }, []);
   return (
     <div>
-      <TestBanner />
-      <TestHealthPackages />
+      <TestBanner 
+        getAllTest={getAllTest} 
+        sortBy={sortBy}
+        setSearch={setSearch}
+      />
+      <TestHealthPackages 
+        testListing={testListing}
+        getAllTest={getAllTest}
+        search={search} setSearch={setSearch}
+        tackTest={tackTest} setTackTest={setTackTest}
+        sortByPrice={sortByPrice}
+        loader={loader} setLoader={setLoader}
+        addTestToCart={addTestToCart}
+      />
       <PopularTests />
       <PopularPackages />
     </div>
