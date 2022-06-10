2f4e9ab485fb1f8fafd9645aee1f4f565d8aed3f
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-06-10 13:38:53 +0530

    Load more Filter

--- src/Components/Test/Sections/TestHealthpackages.jsx
+++ src/Components/Test/Sections/TestHealthpackages.jsx
@@ -1,31 +1,10 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React  from "react";
 import { Link } from "react-router-dom";
-import { setAllTestDetails } from "../../../Redux/Actions/TestAction";
-import { API_URL } from "../../../Redux/Constant/ApiRoute";
 import TestCard from "../../Containers/TestCardComponent";
 import loaderGif from '../../../assets/images/loader-2.gif'
 
-export default function TestHealthPackages() {
-  const dispatch                 = useDispatch();
-  const testListing              = useSelector((state) => state.TestList.testList);
-  const [tackTest, setTackTest]  = useState(4);
-  const [loader, setLoader]      = useState(true);
-
-  const getAllTest = () => { 
-    setLoader(true)
-    axios.post(API_URL.TEST_LISTS, {
-      tack: tackTest,
-    }).then((response) => {
-      dispatch(setAllTestDetails(response.data.data));
-      setLoader(false)
-    });
-  };
-  useEffect(() => {
-      getAllTest();
-  }, []);
- 
+export default function   TestHealthPackages(props) {
+    
   return (
     <section className="">
       <div className="container">
@@ -46,32 +25,35 @@ export default function TestHealthPackages() {
             <div className="col">
               <div className="serch-filter text-right">
                 Sort By
-                <select className="form-control " id="seek" name="seek">
-                  <option value="Orthopaedic Care">Price : Low-High</option>
-                  <option value="Orthopaedic Care">Price : High-Low</option>
+                <select value={props.sortBy} className="form-control" onChange={(e) => props.sortByPrice(e.target.value) } >
+                  <option value=""> -- Choose --</option>
+                  <option value="high"> Price : High-Low</option>
+                  <option value="low">  Price : Low-High</option>
                 </select>
               </div>
             </div>
           </div>
         </div>
         <div className="row">
-          {testListing !== undefined
-            ? testListing.map((test, index) => (
+          { 
+            props.testListing !== undefined
+            ? props.testListing.map((test, index) => (
                 <div className="col-lg-3" key={index}>
-                  <TestCard data={test}/>
+                  <TestCard test={test} addTestToCart={props.addTestToCart}/>
                 </div>
               ))
-            : null}
+            : null
+          }
         </div>
         <div className="load-mrebtn text-center">
           <a onClick={() => {
-            setTackTest(tackTest + 4);
-            getAllTest();
+            props.getAllTest(props.sortBy, props.search, props.tackTest + 4);
+            props.setTackTest(props.tackTest + 4)
           }}>
             Load More
           </a>
           {
-            loader === true ? 
+            props.loader === true ? 
               <h1><img src={loaderGif} alt="loader" /></h1>
             : null
           }
