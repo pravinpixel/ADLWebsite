d75f2de6fa205e4d75ac417995d6b86c10e954d8
behin-pixel <behin.pixel@gmail.com>
2022-06-30 17:24:32 +0530

    physiotheraphy page

---
+++ src/Components/Physiotherapy/Physiotherapy.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Physiotherapy() {
+  return (
+    <div>Physiotherapy</div>
+  )
+}
--- src/Redux/Constant/ApiRoute.jsx
+++ src/Redux/Constant/ApiRoute.jsx
@@ -1,4 +1,5 @@
-export const URL = "http://192.168.0.59:8080/ADL/WebApp/ADL"
+// export const URL = "http://192.168.0.59:8080/ADL/WebApp/ADL"
+export const URL = "http://adladmin.pixel-studios.net"
 
 export const API_URL = {
     ASSETS              : `${URL}/storage/app/`,
@@ -6,5 +7,5 @@ export const API_URL = {
     TOP_BOOKED_TEST     : `${URL}/public/api/topBookedTest`,
     TEST_DETAILS        : `${URL}/api/test/`,
     TEST_LISTS          : `${URL}/api/test-list`,
-    BANNER_FROM        : `${URL}/api/bannerContactForm`,
+    BANNER_FROM         : `${URL}/api/bannerContactForm`,
 }
