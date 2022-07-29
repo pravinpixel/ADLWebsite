42cdcad30a46e249eb30633f35e6c16ea2ee95b6
behin-pixel <behin.pixel@gmail.com>
2022-07-29 19:06:52 +0530

    Bkp

--- Bkp/26-07-2022/src/Redux/Actions/BannerAction.jsx
+++
@@ -1,8 +0,0 @@
-import { ActionTypes } from "../Constant/ActionTypes"
-
-export const  setBanners = (banners) => {
-    return {
-        type    : ActionTypes.SET_BANNERS,
-        payload : banners
-    }
-} 
