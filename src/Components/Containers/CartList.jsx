c22d42cddea92b32bdeac63412d5119870dc2540
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-12-20 10:19:18 +0530

    Cart List Empty Page Condition

--- src/Components/Containers/CartList.jsx
+++ src/Components/Containers/CartList.jsx
@@ -4,10 +4,10 @@ import { RiDeleteBinLine } from "react-icons/ri";
 import Sliders from "react-slick";
 import TestCard from "./TestCardComponent";
 import PackageCard from "./PackageCardComponent";
-import  toast  from "react-hot-toast";
+import toast from "react-hot-toast";
 import { useDispatch } from "react-redux";
 import { setTestCartList } from "../../Redux/Actions/TestAction";
-import emptyCart from '../.././assets/images/cart_empty.png'
+import emptyCart from "../.././assets/images/cart_empty.png";
 
 export default function CartList() {
   var settings = {
@@ -35,7 +35,7 @@ export default function CartList() {
       {
         breakpoint: 900,
         settings: {
-          slidesToShow: 3, 
+          slidesToShow: 3,
         },
       },
       {
@@ -47,46 +47,50 @@ export default function CartList() {
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 1, 
+          slidesToShow: 1,
         },
       },
     ],
   };
 
-  const [CartTable , setCartTable] = useState([]);
-  const [testTotal , setTestTotal] = useState(0)
+  const [CartTable, setCartTable] = useState([]);
+  const [testTotal, setTestTotal] = useState(0);
   const dispatch = useDispatch();
-    
-  
+
   useEffect(() => {
-    window.scroll(0,0)
-      setCartTable(JSON.parse(localStorage.getItem('CartTestList')));
-      var testListFromCart = JSON.parse(localStorage.getItem('CartTestList'))
-      if(testListFromCart != null) {
-        console.log(testListFromCart)
-        const CalculateTotalTestPrice = testListFromCart.reduce((previousValue, currentValue) => parseInt(previousValue) + parseInt(currentValue.TestPrice), 0)
-        localStorage.setItem('cartItemTotal',  CalculateTotalTestPrice);
-        setTestTotal(CalculateTotalTestPrice)
-      } else {
-        window.location.href = "/";
-      }
-   }, [])
-  
+    window.scroll(0, 0);
+    setCartTable(JSON.parse(localStorage.getItem("CartTestList")));
+    var testListFromCart = JSON.parse(localStorage.getItem("CartTestList"));
+    if (testListFromCart != null) {
+      console.log(testListFromCart);
+      const CalculateTotalTestPrice = testListFromCart.reduce(
+        (previousValue, currentValue) =>
+          parseInt(previousValue) + parseInt(currentValue.TestPrice),
+        0
+      );
+      localStorage.setItem("cartItemTotal", CalculateTotalTestPrice);
+      setTestTotal(CalculateTotalTestPrice);
+    } else {
+      window.location.href = "/";
+    }
+  }, []);
+
   const removeCartItem = (index) => {
     CartTable.splice(index, 1);
-    localStorage.setItem('CartTestList', JSON.stringify([...CartTable]));
+    localStorage.setItem("CartTestList", JSON.stringify([...CartTable]));
     setCartTable([...CartTable]);
-    dispatch(setTestCartList(JSON.parse(localStorage.getItem('CartTestList'))));
- 
- 
-    const CalculateTotalTestPrice = CartTable.reduce(
-      (previousValue, currentValue) => parseInt(previousValue) + parseInt(currentValue.TestPrice),0
-    )
-    localStorage.setItem('cartItemTotal',  CalculateTotalTestPrice);
-    setTestTotal(CalculateTotalTestPrice)
+    dispatch(setTestCartList(JSON.parse(localStorage.getItem("CartTestList"))));
 
-    toast.error('Item has been Removed!');
-  }
+    const CalculateTotalTestPrice = CartTable.reduce(
+      (previousValue, currentValue) =>
+        parseInt(previousValue) + parseInt(currentValue.TestPrice),
+      0
+    );
+    localStorage.setItem("cartItemTotal", CalculateTotalTestPrice);
+    setTestTotal(CalculateTotalTestPrice);
+
+    toast.error("Item has been Removed!");
+  };
 
   return (
     <>
@@ -110,101 +114,114 @@ export default function CartList() {
           </div>
         </div>
       </section>
-      <section className="total-carting">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12">
-              <div className="common-heading">
-                <h2>
-                  <span>Package/Test </span> Selected
-                </h2>
+      {CartTable.length !== 0 ? (
+        <section className="total-carting">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12">
+                <div className="common-heading">
+                  <h2>
+                    <span>Package/Test </span> Selected
+                  </h2>
+                </div>
               </div>
-            </div>
-            <div className="col-lg-8">
-              <div className="cart-ing table-responsive">
-                {
-                  CartTable.length !== null  ? 
-                    CartTable.length !== 0 ?
-                      <table className="table">
-                        <thead>
-                          <tr>
-                            <th>Package/Test</th>
-                            <th className="text-right">Type</th>
-                            <th className="text-right">Unit Price(₹)</th>
-                            <th className="text-center">Discount</th>
-                            <th className="text-right">Net Price</th>
-                            <th className="text-center">Action</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {
-                            CartTable.map((item, index) => (
-                              <tr key={index}>
-                                <th scope="row">{item.TestName}</th>
-                                <td className="text-right">Test</td>
-                                <td className="text-right">&#8377;{item.TestPrice}</td>
-                                <td className="text-center">-</td>
-                                <td className="text-right">
-                                  &#8377; {item.TestPrice} 
-                                </td>
-                                <td className="text-center clr-chng">
-                                  <Link to="">
-                                    <RiDeleteBinLine className="text-danger" onClick={() => removeCartItem(index) } />
-                                  </Link>
-                                </td>
-                              </tr>
-                            ))
-                            }
-                        </tbody>
-                      </table>
-                    :  <span>
-                        <img src={emptyCart} alt="Cart is Empty" />
-                      </span>
-                  : null
-                }
-              </div>
-              <div className="case">
-                <p>
-                  <Link to="/for-patient">Add More Test</Link>
-                  <Link className="bg-trsnper" to="/for-patient">
-                    Add More Package
-                  </Link>
-                </p>
-              </div>
-            </div>
-            <div className="col-lg-4">
-              <div className="availab-lity ca-rtloc">
-                <h4>Payment</h4>
-                <table className="table">
-                  <tbody>
-                  <tr>
-                    <th className="text-left">Subtotal</th>
-                    <th className="text-right">&#8377;{testTotal}</th>
-                  </tr>
-                  <tr>
-                    <td className="text-left">Discount (-)</td>
-                    <td className="text-right">- &#8377; 0</td>
-                  </tr>
-                  <tr>
-                    <td className="text-left">
-                      <b>Total</b>
-                    </td>
-                    <td className="text-right">
-                      <b>&#8377;{testTotal}</b>
-                    </td>
-                  </tr>
-                  </tbody>
-                </table>
-                <div className="case text-right">
+              <div className="col-lg-8">
+                <div className="cart-ing table-responsive">
+                  <table className="table">
+                    <thead>
+                      <tr>
+                        <th>Package/Test</th>
+                        <th className="text-right">Type</th>
+                        <th className="text-right">Unit Price(₹)</th>
+                        <th className="text-center">Discount</th>
+                        <th className="text-right">Net Price</th>
+                        <th className="text-center">Action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {CartTable.map((item, index) => (
+                        <tr key={index}>
+                          <th scope="row">{item.TestName}</th>
+                          <td className="text-right">
+                            {item.IsPackage === 'Yes' ? 'Package' : 'Test'}
+                          </td>
+                          <td className="text-right">
+                            &#8377;{item.TestPrice}
+                          </td>
+                          <td className="text-center">-</td>
+                          <td className="text-right">
+                            &#8377; {item.TestPrice}
+                          </td>
+                          <td className="text-center clr-chng">
+                            <Link to="">
+                              <RiDeleteBinLine
+                                className="text-danger"
+                                onClick={() => removeCartItem(index)}
+                              />
+                            </Link>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+                <div className="case">
                   <p>
-                    <Link to="/">Checkout</Link>
+                    <Link to="/for-patient">Add More Test</Link>
+                    <Link className="bg-trsnper" to="/for-patient">
+                      Add More Package
+                    </Link>
                   </p>
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className="availab-lity ca-rtloc">
+                  <h4>Payment</h4>
+                  <table className="table">
+                    <tbody>
+                      <tr>
+                        <th className="text-left">Subtotal</th>
+                        <th className="text-right">&#8377;{testTotal}</th>
+                      </tr>
+                      <tr>
+                        <td className="text-left">Discount (-)</td>
+                        <td className="text-right">- &#8377; 0</td>
+                      </tr>
+                      <tr>
+                        <td className="text-left">
+                          <b>Total</b>
+                        </td>
+                        <td className="text-right">
+                          <b>&#8377;{testTotal}</b>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <div className="case text-right">
+                    <p>
+                      <Link to="/">Checkout</Link>
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+        </section>
+      ) : (
+        <div style={{ backgroundColor:'#f9f9f9' }} className="text-center">
+          <div className="case rounded-0">
+            <img src={emptyCart} alt="Cart is Empty" className="my-5"/> 
+            <p>
+              <Link to="/for-patient">Add Test</Link>
+              <Link className="bg-trsnper" to="/for-patient">
+                Add Package
+              </Link>
+            </p>
+          </div>
+          <hr className="w-75 mx-auto my-0" />
         </div>
-      </section>
+      )}
+
       <section className="diagnostics most-poptst text-left">
         <div className="container">
           <div className="row">
