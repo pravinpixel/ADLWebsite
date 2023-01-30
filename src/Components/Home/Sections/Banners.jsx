24b7074f759b83fc76061c82fd7685db89e99c20
behin-pixel <behin.pixel@gmail.com>
2023-01-30 15:47:41 +0530

    Banner Change

--- src/Components/Home/Sections/Banners.jsx
+++ src/Components/Home/Sections/Banners.jsx
@@ -1,16 +1,15 @@
-import { useMemo } from 'react'
+import axios from 'axios'
+import { useMemo }  from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { API_URL } from '../../../Redux/Constant/ApiRoute'
-import { assets } from '../../../../src/Helpers'
+import {assets} from '../../../../src/Helpers'
 import { setBanners } from '../../../Redux/Actions/BannerAction'
-import { setLoading } from '../../../Redux/Actions/LoaderAction'
-import Carousel from 'react-bootstrap/Carousel';
 import BannerForm from './BannerForm'
-import axios from 'axios'
+import { setLoading } from '../../../Redux/Actions/LoaderAction'
 
-const Banners = () => {
+const Banners = () => { 
     const dispatch = useDispatch();
-    const bannerList = useSelector((state) => state.HomePageBanners)
+    const bannerList = useSelector((state) =>  state.HomePageBanners)
     const getBanners = () => {
         dispatch(setLoading(true))
         axios.get(API_URL.BANNERS).then((response) => {
@@ -18,42 +17,29 @@ const Banners = () => {
             dispatch(setBanners(response.data))
         });
     }
-    useMemo(() => getBanners(), []);
-    return (
-        <>
-            <div className="banner-conceptual">
-                <div id="home-banner-carousel" className="carousel slide carousel-fade" data-ride="carousel">
-                    {
-                        bannerList.length === 0 ? null :
-                        <ul className="carousel-indicators">
-                            {
-<<<<<<< Updated upstream
-                                bannerList.banners.data.map((banner, index) => (
-                                    <li key={index} data-target="#home-banner-carousel" data-slide-to={index} className={index == 0 ? "active" : null}></li>
-                                ))
-                            }
-                        </ul>
-                    }
-                    <div className="carousel-inner" style={{ "background": "linear-gradient(#503390,#7e2e91)" }}>
+    useMemo(() => getBanners(), []); 
+    return ( 
+        <div className="banner-conceptual">
+            <div id="home-banner-carousel" className="carousel slide carousel-fade" data-ride="carousel">
+                {
+                    bannerList.length === 0 ?   null    : 
+                    <ul className="carousel-indicators"> 
                         {
-                            bannerList.length === 0
-                                ?
-                                <div className="carousel-item active"></div>
-                                :
-                                <>
-                                    {
-                                        bannerList.banners.data.map((banner, index) => (
-                                            <div key={index} className={index === 0 ? "carousel-item active" : "carousel-item"} style={{ "background": `url(${assets(window.innerWidth > 600 ? banner.DesktopImage : banner.MobileImage)})` }}>
-                                                <div className="container">
-                                                    <div className="row">
-                                                        <div className="col-sm-12 col-md-12 col-lg-12">
-                                                            <div className="carousel-caption">
-                                                                <h1>{banner?.Title ?? ""}</h1>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-=======
+                            bannerList.banners.data.map(( banner, index ) =>  (
+                                <li key={index} data-target="#home-banner-carousel" data-slide-to={index} className={index == 0 ? "active" : null}></li>
+                            ))
+                        }
+                    </ul>
+                }
+                 
+                <div className="carousel-inner" style={{"background" : "linear-gradient(#503390,#7e2e91)"}}>
+                    {
+                        bannerList.length === 0
+                        ? 
+                            <div className="carousel-item active"></div> 
+                        : 
+                        <>
+                            {
                                 bannerList.banners.data.map(( banner, index ) =>  ( 
                                     <div key={index} className={index === 0 ? "carousel-item active" : "carousel-item"} style={{ "background": `url()` }}> 
                                             <div className="row">
@@ -62,19 +48,22 @@ const Banners = () => {
                                                 <img className="w-100" src={`${assets(window.innerWidth > 980 ? banner.DesktopImage : banner.MobileImage)}`} /> 
                                                 
                                         
->>>>>>> Stashed changes
                                             </div>
-                                        ))
-                                    }
-                                </>
-                        }
-                    </div>
-                </div>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </>
+                    } 
+                </div> 
+
+
+            </div> 
+            
                 {/* ========= Banner Form ========= */}
                 <BannerForm />
                 {/* ========= End : Banner Form ========= */}
-            </div>
-        </>
+        </div>   
     )
 }
 export default Banners
