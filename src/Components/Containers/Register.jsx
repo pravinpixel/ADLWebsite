2091a985cafba6813bbf15e7740f734a5e92a306
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-02-06 12:50:28 +0530

    Register Form Validation Issue

--- src/Components/Containers/Register.jsx
+++ src/Components/Containers/Register.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { useState } from "react";
-import { Form } from "react-component-form";
 import { Link } from "react-router-dom";
 import { API_URL } from "../../Redux/Constant/ApiRoute";
 import AuthUser, { PutUser } from "../../Helpers/AuthUser";
@@ -9,60 +8,46 @@ import { toast } from "react-hot-toast";
 import { useDispatch } from "react-redux";
 import { setLoading } from "../../Redux/Actions/LoaderAction";
 import { setAuthUser } from "../../Redux/Actions/TestAction";
-
+import { useForm } from "react-hook-form";  
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as Yup from 'yup'
 
 export default function Login() {
-  const [name, setName] = useState(null);
-  const [email, setEmail] = useState(null);
-  const [password, setPassword] = useState(null);
-  const [confirmPassword, setConfirmPassword] = useState(null);
+  
   let navigate = useNavigate();
   const dispatch = useDispatch()
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    if (name === "name") {
-      setName(value);
-    }
-    if (name === "email") {
-      setEmail(value);
-    }
-    if (name === "password") {
-      setPassword(value);
-    }
-    if (name === "confirm_password") {
-      setConfirmPassword(value);
-    }
-  };
- 
-  const handleSubmit = () => {
+  
+  const formSchema = Yup.object().shape({
+    name           : Yup.string().required(),
+    moblie         : Yup.string().required(),
+    email          : Yup.string().required(),
+    password       : Yup.string().required().min(3),
+    confirmPassword: Yup.string().required().oneOf([Yup.ref('password')], 'Passwords does not match'),
+  })
+  const formOptions = { resolver: yupResolver(formSchema) }
+  const { register, handleSubmit, reset, formState } = useForm(formOptions)
+  const { errors } = formState
+  function RegisterNow(data) {
     dispatch(setLoading(true))
-    if(password === confirmPassword) {
-      axios.post(API_URL.REGISTER,{
-        name    : name,
-        email   : email,
-        password: password,
-      }).then((response) => {
-        dispatch(setLoading(false))
-        if(response.data.status) {
-          PutUser({
-            email : response.data.data.email,
-            id : response.data.data.id,
-            name : response.data.data.name
-          })
-          dispatch(setAuthUser(response.data.data))
-          if(AuthUser()) {
-            navigate("/my-cart");
-          }
-        } else {
-          toast.error(response.data.message)
-        }
-      })
-    } else {
+    axios.post(API_URL.REGISTER, data).then((response) => {
       dispatch(setLoading(false))
-      toast.error('Passwords did not match !')
-    }
-  };
+      if (response.data.status) {
+        dispatch(setLoading(false))
+        PutUser({
+          email: response.data.data.email,
+          id: response.data.data.id,
+          name: response.data.data.name
+        })
+        dispatch(setAuthUser(response.data.data))
+        if (AuthUser()) {
+          navigate("/my-account");
+        }
+        toast.success('Registration Success!')
+      } else {
+        toast.error(response.data.message)
+      }
+    }) 
+  } 
 
   return (
     <div>
@@ -75,7 +60,6 @@ export default function Login() {
                   <div className="dhoni-bgm">
                     <div className="common-heading">
                       <h2>
-                        
                         Welcome <span> to Neuberg Anand </span>
                       </h2>
                     </div>
@@ -83,68 +67,35 @@ export default function Login() {
                 </div>
                 <div className="col-lg-6">
                   <div className="cir-frm">
-                    <Form>
+                    <form onSubmit={handleSubmit(RegisterNow)}>
                       <div className="frm-fields row clearfix">
                         <div className="col-lg-12 col-md-12 col-sm-12">
                           <div className="common-heading">
-                            <h2>
-                              
-                              Register <span> Here! </span>
-                            </h2>
+                            <h2>Register <span> Here! </span></h2>
                           </div>
                           <div className="row">
                             <div className="form-data col-lg-12">
-                              <input
-                                className="input100"
-                                type="text"
-                                name="name"
-                                placeholder="Name"
-                                required
-                                onChange={(e) => handleInputChange(e)}
-                              />
+                              <input className={`input1001 ${errors.name && 'border-danger'}`} type="text" placeholder="Name *" {...register("name")} /> 
                             </div>
                             <div className="form-data col-lg-12">
-                              <input
-                                className="input100"
-                                type="text"
-                                name="email"
-                                placeholder="E-mail"
-                                required
-                                onChange={(e) => handleInputChange(e)}
-                              />
+                              <input className={`input1001 ${errors.moblie && 'border-danger'}`} type="text" placeholder="Moblie *" {...register("moblie")} />
                             </div>
                             <div className="form-data col-lg-12">
-                              <input
-                                className="input100"
-                                type="password"
-                                name="password"
-                                placeholder="Password"
-                                required
-                                onChange={(e) => handleInputChange(e)}
-                              />
+                              <input className={`input1001 ${errors.email && 'border-danger'}`} type="email" placeholder="E-mail *" {...register("email")} />
                             </div>
                             <div className="form-data col-lg-12">
-                              <input
-                                className="input100"
-                                type="password"
-                                name="confirm_password"
-                                placeholder="Re-Enter your Password"
-                                required
-                                onChange={(e) => handleInputChange(e)}
-                              />
+                              <input className={`input1001 ${errors.password && 'border-danger'}`} type="password" placeholder="Password *" {...register("password")} />
+                            </div>
+                            <div className="form-data col-lg-12">
+                              <input className={`input1001 ${errors.confirmPassword && 'border-danger'}`} type="password"  {...register("confirmPassword")} placeholder="Re-Enter your Password *" />
                             </div>
                             <div className="form-data sbm col-lg-12">
-                              <input
-                                onClick={()=>handleSubmit()} 
-                                type="submit"
-                                name="submit"
-                                value="REGISTER"
-                              />
+                              <input type="submit" name="submit" value="REGISTER" />
                             </div>
                           </div>
                         </div>
                       </div>
-                    </Form>
+                    </form>
                     <div className="col-lg-12 text-center mt-3">
                       <div className="user-regster">
                         Already have a account ?<Link to="/login"> Login</Link>
