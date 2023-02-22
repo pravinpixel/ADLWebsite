d20830e9b49745f381767eaf343e0e5b480385ee
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-02-22 13:03:07 +0530

    Cart List Responsive UI Creation

--- src/Components/Containers/CartList.jsx
+++ src/Components/Containers/CartList.jsx
@@ -18,8 +18,8 @@ export default function CartList() {
   const fetchCartList = () => {
     var AuthID = JSON.parse(localStorage.getItem('user')).id;
     axios.post(`${API_URL.CUSTOMER_CART_ITEMS}/${AuthID}`).then((response) => {
-      if(localStorage.getItem("CartTestList") == undefined || localStorage.getItem("CartTestList") == null) {
-        localStorage.setItem("CartTestList",JSON.stringify(response.data))
+      if (localStorage.getItem("CartTestList") == undefined || localStorage.getItem("CartTestList") == null) {
+        localStorage.setItem("CartTestList", JSON.stringify(response.data))
         dispatch(setTestCartList(response.data));
       }
       setCartTable(response.data);
@@ -94,136 +94,121 @@ export default function CartList() {
         </div>
       </section>
       {CartTable.length !== 0 ? (
-        <section className="total-carting">
+        <section className="total-carting pt-3">
           <div className="container">
             <div className="row">
-              <div className="col-lg-12">
-                <div className="common-heading">
-                  <h2>
-                    <span>Package/Test </span> Selected
-                  </h2>
-                </div>
-              </div>
-              <div className="col-lg-12">
-                <div className="cart-ing table-responsive">
-                  <table className="table">
-                    <thead>
-                      <tr>
-                        <th>Package/Test</th>
-                        <th className="text-right">Type</th>
-                        <th className="text-right">Unit Price(₹)</th>
-                        <th className="text-right">Net Price</th>
-                        <th className="text-center">Action</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {CartTable.map((item, index) => (
-                        <tr key={index}>
-                          <th scope="row">{item.TestName}</th>
-                          <td className="text-right">
-                            {item.IsPackage === "Yes" ? <span className="px-2 py-1 badge badge-success"><i className="fa fa-flask" aria-hidden="true"></i> Package </span> : <span className="px-2 py-1 badge badge-primary"><i className="fa fa-flask" aria-hidden="true"></i> Test </span>}
-                          </td>
-                          <td className="text-right">
-                            &#8377;{item.TestPrice}
-                          </td>
-                          <td className="text-right">
-                            &#8377; {item.TestPrice}
-                          </td>
-                          <td className="text-center clr-chng">
-                            <Link to="">
-                              <RiDeleteBinLine
-                                className="text-danger"
-                                onClick={() => removeCartItem(index, item.cart_id)}
-                              />
-                            </Link>
+              {
+                window.innerWidth > 980
+                  ?
+                  <div className="cart-ing table-responsive">
+                    <table className="table">
+                      <thead>
+                        <tr>
+                          <th>Package/Test</th>
+                          <th className="text-right">Type</th>
+                          <th className="text-right">Unit Price(₹)</th>
+                          <th className="text-right">Net Price</th>
+                          <th className="text-center">Action</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {CartTable.map((item, index) => (
+                          <tr key={index}>
+                            <th scope="row">{item.TestName}</th>
+                            <td className="text-right">
+                              {item.IsPackage === "Yes" ? <span className="px-2 py-1 badge badge-success"><i className="fa fa-flask" aria-hidden="true"></i> Package </span> : <span className="px-2 py-1 badge badge-primary"><i className="fa fa-flask" aria-hidden="true"></i> Test </span>}
+                            </td>
+                            <td className="text-right">
+                              &#8377;{item.TestPrice}
+                            </td>
+                            <td className="text-right">
+                              &#8377; {item.TestPrice}
+                            </td>
+                            <td className="text-center clr-chng">
+                              <Link to="">
+                                <RiDeleteBinLine
+                                  className="text-danger"
+                                  onClick={() => removeCartItem(index, item.cart_id)}
+                                />
+                              </Link>
+                            </td>
+                          </tr>
+                        ))}
+                        <tr>
+                          <td className="text-right" colSpan={3}><b style={{ fontSize: '16px' }}>Total</b></td>
+                          <td className="text-right"><b style={{ fontSize: '16px' }}>₹ {testTotal}</b></td>
+                          <td>
+                            <div className="case m-0 text-center">
+                              <p>
+                                <Link to="/checkout">Checkout</Link>
+                              </p>
+                            </div>
                           </td>
                         </tr>
-                      ))}
-                      <tr>
-                        <td className="text-right" colSpan={3}><b style={{ fontSize: '16px' }}>Total</b></td>
-                        <td className="text-right"><b style={{ fontSize: '16px' }}>₹ {testTotal}</b></td>
-                        <td>
-                          <div className="case m-0 text-center">
-                            <p>
-                              <Link to="/checkout">Checkout</Link>
-                            </p>
-                          </div>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-                {/* <div className="only-mobileresponsive">
-                  <div className="box-fisherr">
-                    <div className="col-lg-12 p-0">
-                      <div className="pack-name">
-                        <h3>Package/Test</h3>
-                        <p>11 DEOXY CORTISOL (LC-MS/MS)</p>
-                        <span className="px-2 py-1 badge badge-primary"><i className="fa fa-flask" aria-hidden="true"></i> Test </span>
-                        <div className="text-center">
-                          <div className="prce-net">
-                            <h4>Unit Price(₹)</h4>
-                            <p>₹1190</p>
-                          </div>
-                          <div className="prce-fnl">
-                            <h4>Net Price</h4>
-                            <p>₹ 1190</p>
-                          </div>
-                        </div>
-                        <Link to="">
-                          <RiDeleteBinLine
-                            className="text-danger"
-                          // onClick={() => removeCartItem(index,item)}
-                          />
+                      </tbody>
+                    </table>
+                  </div>
+                  :
+                  <div className="col-12">
+                    <div className="only-mobileresponsive">
+                      <div className="col-lg-12 p-0 ">
+                        <ul className="list-group">
+                          {CartTable.map((item, index) => (
+                            <li className="list-group-item" key={index}>
+                              <h6>
+                                {item.IsPackage === "Yes" ?
+                                  <small className="px-2 py-1 badge badge-success"><i className="fa fa-flask" aria-hidden="true"></i> Package </small> :
+                                  <small className="px-2 py-1 badge badge-primary"><i className="fa fa-flask" aria-hidden="true"></i> Test </small>}
+                              </h6>
+                              <h6 className="my-2">{item.TestName}</h6>
+                              <div className="d-flex w-100 justify-content-between">
+                                <p className="m-0"><b>₹ {item.TestPrice} </b></p>
+                                <button className="btn-light border-0 outline-0 rounded pt-2" style={{ cursor: 'pointer' }}>
+                                  <RiDeleteBinLine
+                                    className="text-danger"
+                                    onClick={() => removeCartItem(index, item.cart_id)}
+                                  />
+                                </button>
+                              </div>
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    </div>
+                    <div className="availab-lity ca-rtloc mt-3">
+                      <h4>Total Amount</h4>
+                      <table className="table">
+                        <tbody>
+                          <tr>
+                            <th className="text-left">Subtotal</th>
+                            <th className="text-right">&#8377;{testTotal}</th>
+                          </tr>
+                          <tr>
+                            <td className="text-left">
+                              <b>Total</b>
+                            </td>
+                            <td className="text-right">
+                              <b>&#8377;{testTotal}</b>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                      <div className="case text-right">
+                        <p>
+                          <Link to="/">Checkout</Link>
+                        </p>
+                      </div>
+                    </div>
+                    <div className="case">
+                      <p className="d-flex justify-content-center">
+                        <Link to="/for-patient">Add More Test</Link>
+                        <Link className="bg-trsnper" to="/packages">
+                          Add More Package
                         </Link>
-                      </div>
+                      </p>
                     </div>
                   </div>
-                  <div className="box-fisherr">
-                    <div className="col-lg-12 p-0">
-                      <div className="pack-name">
-                        <h3>Total</h3>
-                        <p>₹ 14280</p>
-                        <div className="case m-0 text-center"><p><a href="/checkout">Checkout</a></p></div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="case">
-                  <p>
-                    <Link to="/for-patient">Add More Test</Link>
-                    <Link className="bg-trsnper" to="/packages">
-                      Add More Package
-                    </Link>
-                  </p>
-                </div> */}
-              </div>
-              {/* <div className="col-lg-4">
-                <div className="availab-lity ca-rtloc">
-                  <h4>Total Amount</h4>
-                  <table className="table">
-                    <tbody>
-                      <tr>
-                        <th className="text-left">Subtotal</th>
-                        <th className="text-right">&#8377;{testTotal}</th>
-                      </tr>
-                      <tr>
-                        <td className="text-left">
-                          <b>Total</b>
-                        </td>
-                        <td className="text-right">
-                          <b>&#8377;{testTotal}</b>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                  <div className="case text-right">
-                    <p>
-                      <Link to="/">Checkout</Link>
-                    </p>
-                  </div>
-                </div>
-              </div> */}
+              }
             </div>
           </div>
         </section>
