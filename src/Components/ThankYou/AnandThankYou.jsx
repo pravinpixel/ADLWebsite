3e392fdee90cf9bdf48a97217b0091a098a67477
Fahd <fahd.pixel@gmail.com>
2023-05-19 11:53:50 +0530

    FEAT: Added Script Tag

--- src/Components/ThankYou/AnandThankYou.jsx
+++ src/Components/ThankYou/AnandThankYou.jsx
@@ -1,22 +1,65 @@
-import { useEffect } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { useEffect } from "react";
+import { Helmet } from "react-helmet";
+import { Link } from "react-router-dom";
 
 function AnandThankYou() {
   useEffect(() => {
-    window.scroll(0,0) 
-  }, [])
+    window.scroll(0, 0);
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", "AW-10841898141");
+  }, []);
   return (
-    <div style={{  display:'flex',justifyContent:'center',alignItems:'center',textAlign:'center',position:'relative',padding:'150px 0px 50px' }}>
-        <img style={{ position:'absolute',top:0,left:0 }} src="https://i.pinimg.com/originals/12/4d/e3/124de3d1b5e12f1d8fcec1685e634361.gif" width="100%" alt="" />
-        <div style={{ zIndex:1 }}>
-            <img src="https://cdn-icons-png.flaticon.com/512/190/190411.png" width="150px" className='mb-4' alt='img' />
-            <h1 >Thank You</h1>
-            <h3 style={{ color:"#1ecdb4" }}className='my-3'>For spending your valuable time. </h3>
-            <p className='mb-4'>We will reach you soon...</p>
-            <Link to="/" style={{ color:'#fdb758' }}><b>Go to Home</b> <i className="fa fa-arrow-right"></i></Link>
+    <>
+      <Helmet>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-10841898141"
+        ></script>
+        {/* <script>
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)} gtag('js', new Date()); gtag('config',
+          'AW-10841898141');
+        </script> */}
+      </Helmet>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+          position: "relative",
+          padding: "150px 0px 50px",
+        }}
+      >
+        <img
+          style={{ position: "absolute", top: 0, left: 0 }}
+          src="https://i.pinimg.com/originals/12/4d/e3/124de3d1b5e12f1d8fcec1685e634361.gif"
+          width="100%"
+          alt=""
+        />
+        <div style={{ zIndex: 1 }}>
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/190/190411.png"
+            width="150px"
+            className="mb-4"
+            alt="img"
+          />
+          <h1>Thank You</h1>
+          <h3 style={{ color: "#1ecdb4" }} className="my-3">
+            For spending your valuable time.{" "}
+          </h3>
+          <p className="mb-4">We will reach you soon...</p>
+          <Link to="/" style={{ color: "#fdb758" }}>
+            <b>Go to Home</b> <i className="fa fa-arrow-right"></i>
+          </Link>
         </div>
-    </div>
-  )
+      </div>
+    </>
+  );
 }
 
-export default AnandThankYou
+export default AnandThankYou;
