1cdd5cca9e361dcda4441c05f6d7ab05ab56a818
behin-pixel <behin.pixel@gmail.com>
2023-06-14 15:15:19 +0530

    New Folder Added

---
+++ captains-health-check/thanks.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      <link rel="canonical" href="https://neubergdiagnostics.com/fullbodycheckup" />
+      <meta name="description" content="Book your Full Body Checkup package with Neuberg Diagnostics">
+      <meta name="keywords" content=" ">
+      <title>Thank You | Neuberg Diagnostics</title>
+      <meta property="og:description" content="" />
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="Neuberg Diagnostics">
+      <link rel="shortcut icon" type="image/png" href="https://neubergdiagnostics.com/static/assets/images/favicon.png"/>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+      <link href="css/style.css" rel="stylesheet">
+	   
+    
+     
+    
+    
+   </head>
+   </head>
+   <body>
+      <div class="page-container">
+         <header class="text-white">
+            <div class="row align-items-center">
+               <div class="col-5">
+                  <a href="https://neubergdiagnostics.com/" target="_blank">
+					<img src="images/logo.png" alt="Logo" class="img-fluid" />
+                  </a>
+               </div>
+               <div class="col-7">
+                  <p class="text-right mb-0">Call now to book<a href="tel:+919700369700"><img src="images/phone-icon.png" alt=""/> +91 9700 36 9700</a></p>
+               </div>
+            </div>
+         </header>
+         <div class="thanks" style="padding: 70px 30px;">
+			<h1 class="text-center text-success mb-0" style="font-size: 60px;"><i class="fa fa-check-circle" aria-hidden="true"></i></h1>
+            <h2 class="text-center pb-2">Your submission has been successful!</h2>
+			<h5 class="text-center">We have received your submission, and we'll be in touch with you soon.</h5>
+         </div>
+         <footer>
+			<div class="row">
+				<div class="col-12 col-sm-12">
+					<div class="ftrlogo"><img src="images/footer-logo.png" alt="" /></div>
+					<p>Neuberg Diagnostics, headquartered in Chennai, is one of India's leading diagnostic service providers, with its presence in India, UAE, South Africa and USA. The laboratories are accredited by CAP* & NABL*. The group has created accessibility to state-of-the art diagnostic technologies and has some of the finest Clinical Pathologists. Biochemists, Geneticists and several other certified clinical lab professionals, with the capability to perform over 6000 varieties of pathological investigations. 
+<br><br>
+The group focuses on prevention, wellness programs and Structured Processing Disease Management programs for rare diseases using the most advanced technologies assisted by Data science and Al tools. Currently. we are processing over 30 million tests every year, generating quality and accurate reports with 150+ labs, 10,000+ touch points in India & other regions.</p>
+				</div>
+
+
+            <div class="col-12 ">
+<div class="location-block text-center">
+
+<h5>Neuberg India</h5>
+
+<p>Andhra Pradesh | Bihar | Chhattisgarh | Delhi NCR | Gujarat | Haryana | Jammu and Kashmir | Karnataka | Kerala I Madhya Pradesh | Maharashtra | Odisha | Punjab | Rajasthan | Tamil Nadu | Telangana | Uttar Pradesh | West Bengal</p>
+<span>
+SOUTH AFRICA | UAE | USA </span>
+
+</div>
+      </div>
+
+
+				<div class="col-12 col-sm-6 ftr-call">
+					<p>Contact Us<a href="tel:+919700369700"><img src="images/footer-phone-icon.png" alt=""/> +91 9700 36 9700</a></p>
+				</div>
+
+             
+
+				<div class="col-12 col-sm-6 copy">
+					<p>&copy; <script>var CurrentYear = new Date().getFullYear()document.write(CurrentYear); </script> Neuberg Diagnostics Pvt. Ltd. <br/>All rights reserved.</p>
+				</div>
+			</div>
+		</footer>
+      </div>
+	  <a href="#" class="mobile-button">Request a call back</a>
+      <span id="top-link-block"><a href="#" class="well well-sm"><i class="fa fa-angle-up" aria-hidden="true"></i></a></span>
+	  
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+      <script src="https://use.fontawesome.com/eafcbbeac7.js"></script>	  
+   </body>
+</html>
